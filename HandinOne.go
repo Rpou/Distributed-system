3feb114ce5ef8796bc728544b46bed63a0b4b42e
Rasmus Mygind Poulsen <rpou@itu.dk>
2024-09-03 12:00:28 +0200

    changeArray

--- HandinOne.go
+++ HandinOne.go
@@ -2,55 +2,54 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"math/rand"
+	"time"
 )
 
-ch1 := make(chan bool)
-forkArry := []bool{false,false,false,false,false}
-forksch := make(chan int)
+var forkArry = []bool
 
 func main() {
+	ch1 := make(chan bool)
+	forksch := make(chan int)
+	forkArry = []bool{false, false, false, false, false}
 	philosipher()
 	philosipher()
 	philosipher()
 	philosipher()
 	philosipher()
 
-	fork()
+	fork(forkArry)
 }
 
-func philosipher(){
+func philosipher() {
 	//how many forks that can be grabbed
 	eating := false
 	forksAva := 0
-	
+
 	//indexes where philosipher has forks
 	indexEating := -1
 	indexEating2 := -1
 	for {
 		for i, v := range forkArry {
-			forksAva = <-forksch
-			if(v == false && forksAva == 1){
+
+			if v == false && forksAva == 1 {
 				forksch <- i
 				indexEating2 = i
-				break;
-			} else if {
+				eating = true
+				break
+			} else if v == false {
 				forksch <- i
 				indexEating = i
 				forksAva++
 			}
-			
-			if forksAva > 1 {
-				eating = true
-			} 	
-			
+
 		}
-	
-		if(indexEating != 1 && forksAva > 0){ //if only got one, put it back
+
+		if indexEating != 1 && forksAva > 0 { //if only got one, put it back
 			forksch <- indexEating
 			indexEating = -1
 		}
-	
+
 		if eating {
 			fmt.Println("eating")
 			forksch <- indexEating //put forks back
@@ -60,21 +59,25 @@ func philosipher(){
 		} else {
 			fmt.Println("thinking")
 		}
-	
-		rand.Seed(time.Now().UnixNano()) //wait between 0 and 1 sec
-		time.Sleep(time.Duration(rand.Intn(1000) * time.Millisecond)) 
-	
-	}	
-}
 
-func fork(){
-	for {
-		index := <- forksch
-		if forkArry[index] {
-			forkArry[index] = true;
-		} else {
-			forkArry[index] = false;
-		}
+		rand.Seed(time.Now().UnixNano()) //wait between 0 and 1 sec
+		time.Sleep(time.Duration(rand.Intn(1000) * time.Millisecond))
+
 	}
 }
 
+func fork(array []bool) {
+	for {
+		index := <-forksch
+		changeArry(index, array)
+	}
+}
+
+func changeArry(index int, array []bool) []bool {
+	if array[index] {
+		array[index] = false
+	} else {
+		array[index] = true
+	}
+	return array
+}
