19baca0932df3fbeda19a5bf1a3439d282e5bc55
Anders0106 <andersgfh@gmail.com>
2024-09-03 10:40:16 +0200

    umiddelbar kode

--- HandinOne.go
+++ HandinOne.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
-	//nemt
+import {
+	"fmt"
+	"sync"
 }
+
+ch1 := make(chan bool)
+
+func main() {
+	philosipher()
+	philosipher()
+	philosipher()
+	philosipher()
+	philosipher()
+
+	fork()
+	fork()
+	fork()
+	fork()
+	fork()
+}
+
+func philosipher(){
+
+
+	if bla bla {
+		fmt.Println("eating")
+	} else {
+		fmt.Println("thinking")
+	}
+}
+
+func fork(){
+	beingUsed := false
+}
+
