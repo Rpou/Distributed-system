070d5dd98b450d1e5595f3f01a462187ed0476ec
Rpou <rpou@itu.dk>
2024-09-18 20:29:13 +0200

    added delay and fail

--- HandinTwo.go
+++ HandinTwo.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -33,12 +34,19 @@ func middleman(ClientToMiddleman chan string, MiddlemanToClient chan string, Ser
 
 		// Client sends a message to middleman, and middleman sends to server
 		case msgFromClientToServer := <-ClientToMiddleman:
-			MiddlemanToServer <- msgFromClientToServer
+			// 10 percent chance func returns false, and won't send message
+			if triesToSendMessagesCouldFail() {
+				triesToSendMessagesCouldDelay()
+				MiddlemanToServer <- msgFromClientToServer
+			}
 
 		// Server sends a message to middleman, and middleman sends to client
 		case msgFromServerToMiddleman := <-ServerToMiddleman:
-			MiddlemanToClient <- msgFromServerToMiddleman
-
+			// 10 percent chance func returns false, and won't send message
+			if triesToSendMessagesCouldFail() {
+				triesToSendMessagesCouldDelay()
+				MiddlemanToClient <- msgFromServerToMiddleman
+			}
 		}
 	}
 }
@@ -48,7 +56,7 @@ func client(ClientToMiddleman chan string, MiddlemanToClient chan string) {
 	//Step 1:
 	// Makes a random SEQ nr for the client and send it to middleman.
 	ClientSeqNr := rand.Intn(8000) + 1
-	fmt.Println("Sending", ClientSeqNr, "to server")
+	fmt.Println("Sending SEQ:", ClientSeqNr, "to server")
 	ClientToMiddleman <- convertIntToString(ClientSeqNr)
 
 	// Step 3
@@ -57,7 +65,7 @@ func client(ClientToMiddleman chan string, MiddlemanToClient chan string) {
 
 	_ = err
 
-	fmt.Println("Recieved:", ServerACKNr, ServerSeqNr)
+	fmt.Println("Recieved:", ServerACKNr, ServerSeqNr, "from the server")
 
 	// the Client recives the ServerSEQ and +1. This is the new ClientACKNr
 	ClientACKNr := ServerSeqNr + 1
@@ -66,7 +74,7 @@ func client(ClientToMiddleman chan string, MiddlemanToClient chan string) {
 	ClientSeqNr = ServerACKNr
 
 	// Sends the new information to the server.
-
+	fmt.Println("Tries to send:", ClientACKNr, ClientSeqNr, "to the server")
 	ClientToMiddleman <- convertIntToString(ClientACKNr) + " " + convertIntToString(ClientSeqNr)
 
 	for i := 0; i < 10; i++ {
@@ -80,6 +88,8 @@ func server(ServerToMiddleman chan string, MiddlemanToServer chan string) {
 	// Step 2
 	// Gets the client SEQ and makes it the servers ACK
 	recivedSeqNr := <-MiddlemanToServer
+	fmt.Println("Server Recived:", recivedSeqNr, "from the client")
+
 	AckNr, err := convertStringToInt(recivedSeqNr)
 	AckNr = AckNr + 1
 	_ = err
@@ -87,15 +97,15 @@ func server(ServerToMiddleman chan string, MiddlemanToServer chan string) {
 	// Makes Client SEQ
 	NewSeqNr := rand.Intn(16000) + 8000 + 1
 
-	fmt.Println(convertIntToString(AckNr) + " " + convertIntToString(NewSeqNr))
+	fmt.Println("Trying to send ACKnr and SEQnr: " + convertIntToString(AckNr) + " " + convertIntToString(NewSeqNr))
 
 	// Sends the servers ACK and SEQ
 	ServerToMiddleman <- convertIntToString(AckNr) + " " + convertIntToString(NewSeqNr)
 
-	fmt.Println("Sending ACKnr and SEQnr")
-
 	//Recives ACK and SEQ From client. Establishes connection
 	RecSeq, RecACK, err := convertStringToTwoInt(<-MiddlemanToServer)
+	fmt.Println("Server Recived:", RecSeq, RecACK, "from the client")
+	fmt.Println("-----------------------------------------------------------------------------------")
 
 	fmt.Println("Connection established!")
 	fmt.Println(RecSeq, RecACK)
@@ -109,6 +119,22 @@ func server(ServerToMiddleman chan string, MiddlemanToServer chan string) {
 		fmt.Println(arry[i])
 	}
 	fmt.Println("Messages finished")
+}
+
+func triesToSendMessagesCouldFail() bool {
+
+	//10% chance it returns false
+	return rand.Float32() < 0.9
+}
+
+func triesToSendMessagesCouldDelay() {
+
+	//10% chance it returns false
+	if rand.Float32() < 0.9 {
+
+	} else { // pause i 1 sec (delay)
+		time.Sleep(3 * time.Second)
+	}
 }
 
 func convertStringToTwoInt(a string) (int, int, error) {
